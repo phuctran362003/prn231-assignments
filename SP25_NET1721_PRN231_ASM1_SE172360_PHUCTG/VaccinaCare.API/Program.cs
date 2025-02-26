b5acb21011f1b7acff50b7f04c287634e0538312
phuctran362003 <trangiaphuc362003181@gmail.com>
2025-02-26 12:47:08 +0700

    seed data in SystemController

--- SP25_NET1721_PRN231_ASM1_SE172360_PHUCTG/VaccinaCare.API/Program.cs
+++ SP25_NET1721_PRN231_ASM1_SE172360_PHUCTG/VaccinaCare.API/Program.cs
@@ -1,8 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using VaccinaCare.Application.Interface;
 using VaccinaCare.Application.Interface.Common;
 using VaccinaCare.Application.Service;
 using VaccinaCare.Application.Service.Common;
+using VaccinaCare.Domain;
 using VaccinaCare.Repository;
 using VaccinaCare.Repository.Commons;
 using VaccinaCare.Repository.Interfaces;
@@ -23,6 +25,7 @@ builder.Services.AddControllers().AddJsonOptions(opt =>
     opt.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
 }
 );
+
 builder.Services.AddScoped<IVaccineService, VaccineService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
@@ -31,13 +34,29 @@ builder.Services.AddScoped<IClaimsService, ClaimsService>();
 builder.Services.AddScoped<ILoggerService, LoggerService>();
 builder.Services.AddHttpContextAccessor();
 
+builder.Services.AddDbContext<VaccinaCareDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    // Redirect to Swagger khi vào trang chủ
+    app.Use(async (context, next) =>
+    {
+        if (context.Request.Path == "/")
+        {
+            context.Response.Redirect("/swagger");
+            return;
+        }
+        await next();
+    });
 }
+
 
 app.UseHttpsRedirection();
 
